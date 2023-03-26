11e524119d63a823fa11dc1464f9e0993b8d6a39
Salvation Kemuel <skemuel007@gmail.com>
2023-03-26 19:16:58 +0100

    Create Repositories for CRUD operations
    Create interface IRepository and IOrderRepository with basic CRUD methods
    Create class Repository that implements IRepository
    Create class IOrderRepository that extends Repository and implements IOrderRepository

---
+++ OrderApi/OrderApi.Data/Database/CustomAzureSqlAuthProvider.cs
@@ -0,0 +1,39 @@
+﻿using Azure.Core;
+using Azure.Identity;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderApi.Data.Database
+{
+    public class CustomAzureSqlAuthProvider : SqlAuthenticationProvider
+    {
+        private static readonly string[] AzureSqlScopes =
+        {
+            "https://database.window.net//.default"
+        };
+
+        private static readonly TokenCredential _credential = new DefaultAzureCredential\90;
+        private readonly string _tenantId;
+
+        public CustomAzureSqlAuthProvider(string tenantId)
+        {
+            _tenantId = tenantId;
+        }
+        public override async Task<SqlAuthenticationToken> AcquireTokenAsync(SqlAuthenticationParameters parameters)
+        {
+            var tokenRequestContext = new TokenRequestContext(AzureSqlScopes, tenantId: _tenantId);
+            var tokenResult = await _credential.GetTokenAsync(tokenRequestContext, default);
+
+            return new SqlAuthenticationToken(tokenResult.Token, tokenResult.ExpiresOn);
+        }
+
+        public override bool IsSupported(SqlAuthenticationMethod authenticationMethod)
+        {
+            return authenticationMethod.Equals(SqlAuthenticationMethod.ActiveDirectoryDeviceCodeFlow);
+        }
+    }
+}
---
+++ OrderApi/OrderApi.Data/Repository/v1/OrderRepository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderApi.Data.Database;
+using OrderApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderApi.Data.Repository.v1
+{
+    public class OrderRepository : Repository<Order>, IOrderRepository
+    {
+        public OrderRepository(OrderContext orderContext) : base(orderContext)
+        {
+        }
+
+        public async Task<List<Order>> GetOrderByCustomerGuidAsync(Guid customerId, CancellationToken cancellationToken = default)
+        {
+            return await _orderContext.Orders.Where(x => x.OrderState == 2).ToListAsync(cancellationToken);
+        }
+
+        public async Task<Order> GetOrderByIdAsync(Guid orderId, CancellationToken cancellationToken = default) =>
+            await _orderContext.Orders.FirstOrDefaultAsync(x => x.Id == orderId, cancellationToken);
+
+        public async Task<List<Order>> GetPaidOrdersAsync(CancellationToken cancellationToken = default)
+        {
+            return await _orderContext.Orders.Where(x => x.OrderState == 2).ToListAsync(cancellationToken);
+        }
+    }
+}
