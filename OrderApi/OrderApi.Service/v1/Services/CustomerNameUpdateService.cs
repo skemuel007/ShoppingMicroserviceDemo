0a32a6aa283e5b72d0e782c4628e59bc485915cf
Salvation Kemuel <skemuel007@gmail.com>
2023-03-30 09:51:55 +0100

    Create Query and Command Handlers

---
+++ OrderApi/OrderApi.Service/v1/Services/CustomerNameUpdateService.cs
@@ -0,0 +1,51 @@
+﻿using MediatR;
+using OrderApi.Service.v1.Command;
+using OrderApi.Service.v1.Models;
+using OrderApi.Service.v1.Query;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderApi.Service.v1.Services
+{
+    public interface ICustomerNameUpdateService
+    {
+        void UpdateCustomerNameInOrders(UpdateCustomerFullNameModel updateCustomerFullNameModel);
+    }
+    public class CustomerNameUpdateService : ICustomerNameUpdateService
+    {
+        private readonly IMediator _mediator;
+
+        public CustomerNameUpdateService(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async void UpdateCustomerNameInOrders(UpdateCustomerFullNameModel updateCustomerFullNameModel)
+        {
+            try
+            {
+                var ordersOfCustomer = await _mediator.Send(new GetOrderByCustomerGuidQuery
+                {
+                    CustomerId = updateCustomerFullNameModel.Id
+                });
+
+                if (ordersOfCustomer.Count != 0) { 
+                    ordersOfCustomer.ForEach( x => x.CustomerFullName = $"{updateCustomerFullNameModel.FirstName} {updateCustomerFullNameModel.LastName}");
+                }
+
+                await _mediator.Send(new UpdateOrderCommand
+                {
+                    Orders = ordersOfCustomer
+                });
+
+            } catch(Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+    }
+}
